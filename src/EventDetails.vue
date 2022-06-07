b43fd58bcfb2f5e10f2b46f642cd1b93d22cb3b4
Jack <83573223+JackT333@users.noreply.github.com>
2022-06-07 23:53:07 +0930

    Add files via upload

---
+++ src/EventDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="event-details">
+    <h1>Event Details</h1>
+    <h2>Contains details from Event Database</h2>
+    <h3>Will contain a calender with the event marked on it</h3>
+    <img src="../assets/calendar-month-view-previous-past-months-1024x682.png">
+  </div>
+</template>
+
+<style scoped>
+.event-details {
+  height: 125vh;
+}
+
+h1,
+h2 {
+  color: #fefae0;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    event: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/Events.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="events">
+    <h1>Events List</h1>
+    <div class="container">
+      <ul class="event-card-container" v-for="item in events" :key="item.id">
+        <!-- <li> -->
+        <EventCard
+          :event="item"
+          v-on:click="
+            {
+            }
+          "
+        />
+        <!-- </li> -->
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.events {
+  height: 100vh;
+}
+
+h1 {
+  font: bold;
+  color: #fefae0;
+}
+
+/*.container {
+
+}*/
+
+.event-card-container {
+  display: inline-block;
+  /* background-color: #606c38; */
+}
+</style>
+
+<script>
+import EventCard from "../components/EventCard.vue";
+// import router from "../router";
+export default {
+  data() {
+    return {
+      events: [
+        {
+          id: 1,
+          name: "Mock event 1",
+          location: "this place",
+          time: "this time",
+          details: "blah blah blah",
+        },
+        {
+          id: 2,
+          name: "Mock event 2",
+          location: "this place",
+          time: "this time",
+          details: "blah blah blah",
+        },
+        {
+          id: 3,
+          name: "Mock event 3",
+          location: "this place",
+          time: "this time",
+          details: "blah blah blah",
+        },
+        {
+          id: 4,
+          name: "Mock event 4",
+          location: "this place",
+          time: "this time",
+          details: "blah blah blah",
+        },
+      ],
+    };
+  },
+
+  components: { EventCard },
+};
+</script>
